b41fc519363f55356b725a29c1f6935880e1c7a9
devaale <evaldas.aleksynas.dev@gmail.com>
2021-01-26 11:36:17 +0200

    Added a-z letter buttons and displayWord() function - shows user correctly guessed letters in the word

--- client/src/components/Game.js
+++ client/src/components/Game.js
@@ -20,8 +20,9 @@ class Game extends Component  {
         })
     }
 
-    handleClick = () => { 
-        axios.get(`/api/game/${this.state.gameData._id}/${'L'}`).then(res => {
+    handleCheckLetter = e => {
+        let letter = e.target.value;
+        axios.get(`/api/game/${this.state.gameData._id}/${letter}`).then(res => {
             this.setState({
                 mistakesMade: res.data.mistakesMade,
                 guessedLetters: res.data.guessedLetters,
@@ -29,16 +30,32 @@ class Game extends Component  {
         })
     }
 
+    displayWord() {
+        return String(this.state.gameData.word).split("").map(letter => (this.state.guessedLetters.includes(letter.toLowerCase()) ? letter : " _ "));
+    }
+
+    generateLetterButtons() {
+        var alphabet = ""
+        for(var i=9;++i<36;) alphabet+=i.toString(36)
+
+        return alphabet.split("").map(letter => (
+            <button key={letter} value={letter} onClick={this.handleCheckLetter}>
+                {letter}
+            </button>
+        ));
+    }
+
     render() {
+        let gameLetterButtons = this.generateLetterButtons();
 
         return (
             <div className="App">
                 <h1>Hangman Game</h1>
                 <h2>Current word: {this.state.gameData.word} and ID: {this.state.gameData._id}</h2>
                 <p>Missed guesses: {this.state.mistakesMade} out of {this.state.gameData.maxMistakes} </p>
-                <p> { this.state.gameData.word } </p>
-                <p>LETTERS</p>
-                <button onClick={this.handleClick}>
+                <p> {this.displayWord()} </p>
+                <p>{gameLetterButtons}</p>
+                <button>
                     TRY AGAIN 
                 </button> 
             </div>
